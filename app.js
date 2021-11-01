0bcb153a7c2dd8fc0cfbcc33de22b3a32a0199f3
JSyndicate <jevwilliamson@gmail.com>
2021-10-31 21:45:16 -0500

    index.html

---
+++ app.js
@@ -0,0 +1,27 @@
+window.onload=function(){
+
+    var searchBtn = document.getElementById("btn")
+    var httpRequest;
+
+    searchBtn.addEventListener('click', function(element) {
+        element.preventDefault();
+
+        httpRequest = new XMLHttpRequest();
+        var url = "http://localhost/info2180-lab4/superheroes.php";
+        httpRequest.onreadystatechange = searchLog;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+
+    });
+
+    function searchLog(){
+        if (httpRequest.readyState == XMLHttpRequest.DONE){
+            if (httpRequest.status == 200){
+               var response = httpRequest.responseText;
+               alert(response);
+            }else{
+                alert('Request Declined.');
+            }
+        }
+    }
+}
